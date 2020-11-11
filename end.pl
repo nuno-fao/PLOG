2565e6dbab5e86f63ff8845b52da4004ad4d0daf
Miguel Pinto <lmiguelapinto@gmail.com>
2020-11-11 00:05:46 +0000

    splitted into two files

---
+++ end.pl
@@ -0,0 +1,88 @@
+game_over(gameState(Board,unusedPieces(R0,B0,R1,B1),OutPieces,_),Winner) :-
+  R0=0,
+  R1=0,
+  B0=0,
+  B1=0,
+  get_number_void(Board,Red,Blue),
+  get_points(P1,P2,Red,Blue,OutPieces),
+  get_winner(P1,P2,Red,Blue,OutPieces,Winner),
+  arg(2,OutPieces,Risk0),
+  arg(3,OutPieces,Risk1),
+  format("Player0 Points: ~p ~p ~p ~n",[P1,Red,Risk0]),
+  format("Player1 Points: ~p ~p ~p ~n",[P2,Blue,Risk1]).
+game_over(_,Winner) :-
+  Winner is -1.
+
+
+
+
+
+get_winner(P1,P2,_,_,_,Winner) :-
+  P1 < P2,
+  Winner is 1,
+  !.
+get_winner(P1,P2,Red,Blue,OutPieces,Winner) :-
+  P2 < P1,
+  Winner is 0,
+  !.
+get_winner(P1,P2,Red,Blue,OutPieces,Winner) :-
+  Red < Blue,
+  Winner is 0,
+  !.
+get_winner(P1,P2,Red,Blue,OutPieces,Winner) :-
+  Blue < Red,
+  Winner is 1,
+  !.
+get_winner(P1,P2,Red,Blue,outPieces(_,_,R0,R1),Winner) :-
+  R0 < R1,
+  Winner is 0,
+  !.
+get_winner(P1,P2,Red,Blue,outPieces(_,_,R0,R1),Winner) :-
+  R1 < R0,
+  Winner is 1,
+  !.
+get_winner(P1,P2,Red,Blue,outPieces(_,_,R0,R1),Winner) :-
+  Winner is 2.
+
+
+
+
+get_points(P1,P2,Red,Blue,outPieces(B0,B1,R0,R1)) :-
+  Red > Blue,
+  P1 is B0 - R0,
+  P2 is B1,
+  !.
+get_points(P1,P2,Red,Blue,outPieces(B0,B1,R0,R1)) :-
+  Blue > Red,
+  P1 is B0,
+  P2 is B1 - R1,
+  !.
+get_points(P1,P2,Red,Blue,outPieces(B0,B1,R0,R1)) :-
+  P1 is B0,
+  P2 is B1.
+
+
+
+
+get_right_elem(L1,Red,Blue) :-
+  same_length(L1,[1]),
+  Red is 0,
+  Blue is 0,
+  !.
+get_right_elem(L1,Red,Blue) :-
+  last(L1,LastE),
+  LastE = ' ',
+  Red is 0,
+  Blue is 0,
+  !.
+get_right_elem(L1,Red,Blue) :-
+  last(L1,LastE),
+  LastE = 'r',
+  Red is 1,
+  Blue is 0,
+  !.
+get_right_elem(L1,Red,Blue) :-
+  last(L1,LastE),
+  LastE = 'b',
+  Red is 0,
+  Blue is 1.
--- game.pl
+++ game.pl
@@ -64,94 +64,9 @@ initial(GameState) :-
 get_player(gameState(_,_,_,Player),Player).
 
 put(GameState).
-    
-game_over(gameState(Board,unusedPieces(R0,B0,R1,B1),OutPieces,_),Winner) :-
-  R0=0,
-  R1=0,
-  B0=0,
-  B1=0,
-  get_number_void(Board,Red,Blue),
-  get_points(P1,P2,Red,Blue,OutPieces),
-  get_winner(P1,P2,Red,Blue,OutPieces,Winner),
-  arg(2,OutPieces,Risk0),
-  arg(3,OutPieces,Risk1),
-  format("Player0 Points: ~p ~p ~p ~n",[P1,Red,Risk0]),
-  format("Player1 Points: ~p ~p ~p ~n",[P2,Blue,Risk1]).
-  
-game_over(_,Winner) :-
-  Winner is -1.
-
-get_winner(P1,P2,_,_,_,Winner) :-
-  P1 < P2,
-  Winner is 1,
-  !.
-
-get_winner(P1,P2,Red,Blue,OutPieces,Winner) :-
-  P2 < P1,
-  Winner is 0,
-  !.
-
-get_winner(P1,P2,Red,Blue,OutPieces,Winner) :-
-  Red < Blue,
-  Winner is 0,
-  !.
-
-get_winner(P1,P2,Red,Blue,OutPieces,Winner) :-
-  Blue < Red,
-  Winner is 1,
-  !.
-get_winner(P1,P2,Red,Blue,outPieces(_,_,R0,R1),Winner) :-
-  R0 < R1,
-  Winner is 0,
-  !.
-get_winner(P1,P2,Red,Blue,outPieces(_,_,R0,R1),Winner) :-
-  R1 < R0,
-  Winner is 1,
-  !.
-get_winner(P1,P2,Red,Blue,outPieces(_,_,R0,R1),Winner) :-
-  Winner is 2.
-
-
-get_points(P1,P2,Red,Blue,outPieces(B0,B1,R0,R1)) :-
-  Red > Blue,
-  P1 is B0 - R0,
-  P2 is B1,
-  !.
-get_points(P1,P2,Red,Blue,outPieces(B0,B1,R0,R1)) :-
-  Blue > Red,
-  P1 is B0,
-  P2 is B1 - R1,
-  !.
-get_points(P1,P2,Red,Blue,outPieces(B0,B1,R0,R1)) :-
-  P1 is B0,
-  P2 is B1.
-
-get_right_elem(L1,Red,Blue) :-
-  same_length(L1,[1]),
-  Red is 0,
-  Blue is 0,
-  !.
-get_right_elem(L1,Red,Blue) :-
-  last(L1,LastE),
-  LastE = ' ',
-  Red is 0,
-  Blue is 0,
-  !.
-get_right_elem(L1,Red,Blue) :-
-  last(L1,LastE),
-  LastE = 'r',
-  Red is 1,
-  Blue is 0,
-  !.
-get_right_elem(L1,Red,Blue) :-
-  last(L1,LastE),
-  LastE = 'b',
-  Red is 0,
-  Blue is 1.
 
 get_number_void(Board,Red,Blue) :-
   get_number_void(Board,Red,Blue,0).
-
 get_number_void([L1|Tail],Red,Blue,N):-
   N>3,
   N<9,
@@ -159,7 +74,6 @@ get_number_void([L1|Tail],Red,Blue,N):-
   !,
   N1 is N +1,
   get_number_void(Tail,Red,Blue,N1).
-
 get_number_void([L1|Tail],Red,Blue,N) :- 
   nth0(0,L1,Piece),
   Piece = 'b',
@@ -172,7 +86,6 @@ get_number_void([L1|Tail],Red,Blue,N) :-
   AuxR is Red1 + Red2,
   Blue is AuxB,
   Red is AuxR.
-
 get_number_void([L1|Tail],Red,Blue,N) :- 
   nth0(0,L1,Piece),
   Piece = 'r',
@@ -185,7 +98,6 @@ get_number_void([L1|Tail],Red,Blue,N) :-
   AuxR is R + 1,
   Blue is AuxB,
   Red is AuxR.
-
 get_number_void([L1|Tail],Red,Blue,N) :- 
   nth0(0,L1,Piece),
   Piece = ' ',
@@ -197,7 +109,6 @@ get_number_void([L1|Tail],Red,Blue,N) :-
   AuxR is Red1 + Red2,
   Blue is AuxB,
   Red is AuxR.
-
 get_number_void(_,Red,Blue,_) :-
   Red is 0,
   Blue is 0.
