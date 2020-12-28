e73330f5124699c0e3389547fee318d4fb31f644
rocas777 <lmiguelapinto@gmail.com>
2020-12-28 06:07:02 +0000

    FUUCKKKK sao 6 da manha

--- TP2/main.pl
+++ TP2/main.pl
@@ -8,8 +8,8 @@ problem(1,
 	[_,_,_,_],
 	[_,_,_,_]],
 	
-    [	[1,4,5,6],
-	[3,2,5,6],
+    [[4,4,5,6],
+	[3,3,5,6],
 	[2,1,6,7],
 	[0,2,7,0]]		
 		).
@@ -20,247 +20,275 @@ problem(2,
     [	[_,_],
 	[_,_]],
 	
-    [	[3,5],
+    [	[4,5],
 	[1,7]]		
 		).
 		
+problem(3,
+
+    [	[_,_,_,_],
+	[_,_,_,_]],
+	
+    [	[4,4,4,4],
+	[0,0,0,0]]		
+		).
+
+
+problem(4,
+
+    [	[_,_],
+	[_,_],
+	[_,_]],
+	
+    [	[3,5],
+	[1,7],
+	[2,6]]		
+		).
+
 tan_and_white(Blank,Dir):-
 	append(Blank,BlankS),
 	append(Dir,DirS),
+	length(DirS,SD),
+	length(BlankS,SB),
+	SD = SB,
+	length(Blank,L),
+	nth0(0,Blank,Linha),
+	length(Linha,C),
 	domain(BlankS,0,1),
-	tan_and_white(BlankS,DirS,0).
-tan_and_white(Blank,Dir,P):-	
+	tan_and_white(BlankS,DirS,0,SD,C,L).
+tan_and_white(Blank,Dir,P,Size,Col,Line):-	
 	S #= 2+Tan,
 	nth0(P,Dir,Direction),
 	nth0(P,Blank,Tan),
-	setC(Blank,Dir,Direction,P,0,S,Tan),
+	setC(Blank,Dir,Direction,P,0,S,Tan,Size,Col,Line),
 	P1 is P +1,
 	!,
-	tan_and_white(Blank,Dir,P1).
-tan_and_white(Blank,Dir,P):- P>3.
+	tan_and_white(Blank,Dir,P1,Size,Col,Line).
+tan_and_white(Blank,Dir,P,Size,Col,Line):- 
+	P>=Size.
+
 	
 %up up
-setC(_,_,0,P,S,S1,_):-	P<0,S1 is S.
-setC(Blank,Dir,0,P,S,SO,Tan):-
+setC(_,_,0,P,S,S1,_,_,_,_):-	P<0,S1 is S.
+setC(Blank,Dir,0,P,S,SO,Tan,Size,Col,Line):-
 	P>=0,
 	nth0(P,Blank,Piece),
 	Piece \= Tan,
-	P1 is P - 2,
-	setC(Blank,Dir,0,P1,S,SO,Tan).
-setC(Blank,Dir,0,P,S,SO,Tan):-
+	P1 is P - Col,
+	setC(Blank,Dir,0,P1,S,SO,Tan,Size,Col,Line).
+setC(Blank,Dir,0,P,S,SO,Tan,Size,Col,Line):-
 	P>=0,
 	nth0(P,Blank,Piece),
 	Piece = Tan,
 	S1 is S+1,
-	P1 is P - 2,
-	setC(Blank,Dir,0,P1,S1,SO,Tan).
+	P1 is P - Col,
+	setC(Blank,Dir,0,P1,S1,SO,Tan,Size,Col,Line).
 	
 %up right
-setC(Blank,Dir,1,P,S,SO,Tan):-
-	X is P mod 2,
-	Y is div(P,2),
-	setC(Blank,Dir,1,X,Y,S,SO,Tan).
-setC(_,_,1,2,_,S,S,_).
-setC(_,_,1,_,-1,S,S,_).
-setC(Blank,Dir,1,X,Y,S,SO,Tan):-
-	X <2,
+setC(Blank,Dir,1,P,S,SO,Tan,Size,Col,Line):-
+	X is P mod Col,
+	Y is div(P,Col),
+	setC(Blank,Dir,1,X,Y,S,SO,Tan,Size,Col,Line).
+setC(_,_,1,X,_,S,S,_,Size,X,Line).
+setC(_,_,1,_,-1,S,S,_,Size,Col,Line).
+setC(Blank,Dir,1,X,Y,S,SO,Tan,Size,Col,Line):-
+	X <Col,
 	X>=0,
 	Y>=0,
-	P1 is Y * 2,
+	P1 is Y * Col,
 	P2 is P1 + X,
 	nth0(P2,Blank,Piece),
 	Piece \= Tan,
 	X1 is X + 1,
 	Y1 is Y - 1,
-	setC(Blank,Dir,1,X1,Y1,S,SO,Tan).
-setC(Blank,Dir,1,X,Y,S,SO,Tan):-
-	X<2,
+	setC(Blank,Dir,1,X1,Y1,S,SO,Tan,Size,Col,Line).
+setC(Blank,Dir,1,X,Y,S,SO,Tan,Size,Col,Line):-
+	X<Col,
 	X>=0,
 	Y>=0,
-	P1 is Y * 2,
+	P1 is Y * Col,
 	P2 is P1 + X,
 	nth0(P2,Blank,Piece),
 	Piece = Tan,
 	S1 is S +1,
 	X1 is X + 1,
 	Y1 is Y - 1,
-	setC(Blank,Dir,1,X1,Y1,S1,SO,Tan).
+	setC(Blank,Dir,1,X1,Y1,S1,SO,Tan,Size,Col,Line).
 	
 %right
-setC(Blank,Dir,2,P,S,SO,Tan):-
+setC(Blank,Dir,2,P,S,SO,Tan,Size,Col,Line):-
 	P>=0,
 	P1 is P + 1,
-	Out is div(P,2),
-	Out1 is div(P1,2),
-	write(Out-Out1),
-	write('\n'),
+	Out is div(P,Col),
+	Out1 is div(P1,Col),
 	Out = Out1,
 	nth0(P,Blank,Piece),
 	Piece \= Tan,
-	setC(Blank,Dir,2,P1,S,SO,Tan).
-setC(Blank,Dir,2,P,S,SO,Tan):-
+	setC(Blank,Dir,2,P1,S,SO,Tan,Size,Col,Line).
+setC(Blank,Dir,2,P,S,SO,Tan,Size,Col,Line):-
 	P>=0,
 	P1 is P + 1,
-	Out is div(P,2),
-	Out1 is div(P1,2),
-	write(Out-Out1),
-	write('\n'),
+	Out is div(P,Col),
+	Out1 is div(P1,Col),
 	Out = Out1,
 	nth0(P,Blank,Piece),
 	Piece = Tan,
 	S1 is S+1,
-	setC(Blank,Dir,2,P1,S1,SO,Tan).
-setC(Blank,Dir,2,P,S,SO,Tan):-
+	setC(Blank,Dir,2,P1,S1,SO,Tan,Size,Col,Line).
+setC(Blank,Dir,2,P,S,SO,Tan,Size,Col,Line):-
 	P>=0,
 	nth0(P,Blank,Piece),
 	Piece = Tan,
 	SO is S + 1.
-setC(_,_,2,P,S,S,_):- write(S),write('\n').
+setC(_,_,2,P,S,S,_,Size,Col,Line):- write(S),write('\n').
 
 %down right
-setC(Blank,Dir,3,P,S,SO,Tan):-
-	X is P mod 2,
-	Y is div(P,2),
-	setC(Blank,Dir,3,X,Y,S,SO,Tan).
-setC(_,_,3,2,_,S,S,_).
-setC(_,_,3,_,2,S,S,_).
-setC(Blank,Dir,3,X,Y,S,SO,Tan):-
-	X <2,
-	Y<2,
+setC(Blank,Dir,3,P,S,SO,Tan,Size,Col,Line):-
+	X is P mod Col,
+	Y is div(P,Col),
+	setC(Blank,Dir,3,X,Y,S,SO,Tan,Size,Col,Line).
+setC(_,_,3,X,_,S,S,_,Size,X,Line).
+setC(_,_,3,_,Y,S,S,_,Size,Col,Y).
+setC(Blank,Dir,3,X,Y,S,SO,Tan,Size,Col,Line):-
+	X <Col,
+	Y<Line,
 	X>=0,
 	Y>=0,
-	P1 is Y * 2,
+	P1 is Y * Col,
 	P2 is P1 + X,
 	nth0(P2,Blank,Piece),
 	Piece \= Tan,
 	X1 is X + 1,
 	Y1 is Y + 1,
-	setC(Blank,Dir,3,X1,Y1,S,SO,Tan).
-setC(Blank,Dir,3,X,Y,S,SO,Tan):-
-	X<2,
-	Y<2,
+	setC(Blank,Dir,3,X1,Y1,S,SO,Tan,Size,Col,Line).
+setC(Blank,Dir,3,X,Y,S,SO,Tan,Size,Col,Line):-
+	X<Col,
+	Y<Line,
 	X>=0,
 	Y>=0,
-	P1 is Y * 2,
+	P1 is Y * Col,
 	P2 is P1 + X,
 	nth0(P2,Blank,Piece),
 	Piece = Tan,
 	S1 is S +1,
 	X1 is X + 1,
 	Y1 is Y + 1,
-	setC(Blank,Dir,3,X1,Y1,S1,SO,Tan).
+	setC(Blank,Dir,3,X1,Y1,S1,SO,Tan,Size,Col,Line).
 	
 	
-	
-setC(_,_,4,P,S,S,_):-	P>3.
-setC(Blank,Dir,4,P,S,SO,Tan):-
-	P>=0,
+%down down
+setC(_,_,4,P,S,S1,_,Size,_,_):-	P>=Size,S1 is S.
+setC(Blank,Dir,4,P,S,SO,Tan,Size,Col,Line):-
+	write(1),
+	P<Size,
 	nth0(P,Blank,Piece),
 	Piece \= Tan,
-	P1 is P + 2,
-	setC(Blank,Dir,4,P1,S,SO,Tan).
-setC(Blank,Dir,4,P,S,SO,Tan):-
-	P>=0,
+	P1 is P + Col,
+	setC(Blank,Dir,4,P1,S,SA,Tan,Size,Col,Line),
+	SO is SA.
+setC(Blank,Dir,4,P,S,SO,Tan,Size,Col,Line):-
+	write(2),
+	P<Size,
 	nth0(P,Blank,Piece),
-	Piece = Tan,
+	Piece == Tan,
 	S1 is S+1,
-	P1 is P + 2,
-	setC(Blank,Dir,4,P1,S1,SO,Tan).
-	
+	P1 is P + Col,
+	setC(Blank,Dir,4,P1,S1,SA,Tan,Size,Col,Line),
+	SO is SA.
 	
 %down left
-setC(Blank,Dir,5,P,S,SO,Tan):-
-	X is P mod 2,
-	Y is div(P,2),
-	setC(Blank,Dir,5,X,Y,S,SO,Tan).
-setC(_,_,5,-1,_,S,S,_).
-setC(_,_,5,_,2,S,S,_).
-setC(Blank,Dir,5,X,Y,S,SO,Tan):-
-	Y<2,
+setC(Blank,Dir,5,P,S,SO,Tan,Size,Col,Line):-
+	X is P mod Col,
+	Y is div(P,Col),
+	setC(Blank,Dir,5,X,Y,S,SO,Tan,Size,Col,Line).
+setC(_,_,5,-1,_,S,S,_,Size,Col,Line).
+setC(_,_,5,_,Y,S,S,_,Size,Col,Y).
+setC(Blank,Dir,5,X,Y,S,SO,Tan,Size,Col,Line):-
+	Y<Line,
 	X>=0,
 	Y>=0,
-	P1 is Y * 2,
+	P1 is Y * Col,
 	P2 is P1 + X,
 	nth0(P2,Blank,Piece),
 	Piece \= Tan,
 	X1 is X - 1,
 	Y1 is Y + 1,
-	setC(Blank,Dir,5,X1,Y1,S,SO,Tan).
-setC(Blank,Dir,5,X,Y,S,SO,Tan):-
-	Y<2,
+	setC(Blank,Dir,5,X1,Y1,S,SO,Tan,Size,Col,Line).
+setC(Blank,Dir,5,X,Y,S,SO,Tan,Size,Col,Line):-
+	Y<Line,
 	X>=0,
 	Y>=0,
-	P1 is Y * 2,
+	P1 is Y * Col,
 	P2 is P1 + X,
 	nth0(P2,Blank,Piece),
 	Piece = Tan,
 	S1 is S +1,
 	X1 is X - 1,
 	Y1 is Y + 1,
-	setC(Blank,Dir,5,X1,Y1,S1,SO,Tan).
+	setC(Blank,Dir,5,X1,Y1,S1,SO,Tan,Size,Col,Line).
 
 %lef
-setC(Blank,Dir,6,P,S,SO,Tan):-
+setC(Blank,Dir,6,P,S,SO,Tan,Size,Col,Line):-
 	P>=0,
 	P1 is P - 1,
-	Out is div(P,2),
-	Out1 is div(P1,2),
-	write(Out-Out1),
-	write('\n'),
+	Out is div(P,Col),
+	Out1 is div(P1,Col),
 	Out = Out1,
 	nth0(P,Blank,Piece),
 	Piece \= Tan,
-	setC(Blank,Dir,6,P1,S,SO,Tan).
-setC(Blank,Dir,6,P,S,SO,Tan):-
+	setC(Blank,Dir,6,P1,S,SO,Tan,Size,Col,Line).
+setC(Blank,Dir,6,P,S,SO,Tan,Size,Col,Line):-
 	P>=0,
 	P1 is P - 1,
-	Out is div(P,2),
-	Out1 is div(P1,2),
-	write(Out-Out1),
-	write('\n'),
+	Out is div(P,Col),
+	Out1 is div(P1,Col),
 	Out = Out1,
 	nth0(P,Blank,Piece),
 	Piece = Tan,
 	S1 is S+1,
-	setC(Blank,Dir,6,P1,S1,SO,Tan).
-setC(Blank,Dir,6,P,S,SO,Tan):-
+	setC(Blank,Dir,6,P1,S1,SO,Tan,Size,Col,Line).
+setC(Blank,Dir,6,P,S,SO,Tan,Size,Col,Line):-
 	P>=0,
 	nth0(P,Blank,Piece),
 	Piece = Tan,
 	SO is S + 1.
-setC(_,_,6,P,S,S,_):- write(S),write('\n').
+setC(_,_,6,P,S,S,_,Size,Col,Line):- write(S),write('\n').
 
 %up left
-setC(Blank,Dir,7,P,S,SO,Tan):-
-	X is P mod 2,
-	Y is div(P,2),
-	setC(Blank,Dir,7,X,Y,S,SO,Tan).
-setC(_,_,7,-1,_,S,S,_).
-setC(_,_,7,_,-1,S,S,_).
-setC(Blank,Dir,7,X,Y,S,SO,Tan):-
+setC(Blank,Dir,7,P,S,SO,Tan,Size,Col,Line):-
+	X is P mod Col,
+	Y is div(P,Col),
+	setC(Blank,Dir,7,X,Y,S,SO,Tan,Size,Col,Line).
+setC(_,_,7,-1,_,S,S,_,Size,Col,Line).
+setC(_,_,7,_,-1,S,S,_,Size,Col,Line).
+setC(Blank,Dir,7,X,Y,S,SO,Tan,Size,Col,Line):-
 	X>=0,
 	Y>=0,
-	P1 is Y * 2,
+	P1 is Y * Col,
 	P2 is P1 + X,
 	nth0(P2,Blank,Piece),
 	Piece \= Tan,
 	X1 is X - 1,
 	Y1 is Y - 1,
-	setC(Blank,Dir,7,X1,Y1,S,SO,Tan).
-setC(Blank,Dir,7,X,Y,S,SO,Tan):-
+	setC(Blank,Dir,7,X1,Y1,S,SO,Tan,Size,Col,Line).
+setC(Blank,Dir,7,X,Y,S,SO,Tan,Size,Col,Line):-
 	X>=0,
 	Y>=0,
-	P1 is Y * 2,
+	P1 is Y * Col,
 	P2 is P1 + X,
 	nth0(P2,Blank,Piece),
 	Piece = Tan,
 	S1 is S +1,
 	X1 is X - 1,
 	Y1 is Y - 1,
-	setC(Blank,Dir,7,X1,Y1,S1,SO,Tan).
+	setC(Blank,Dir,7,X1,Y1,S1,SO,Tan,Size,Col,Line).
 	
 
 %spy(setC),problem(2,A,B),tan_and_white(A,B).
+%problem(1,A,B),tan_and_white(A,B).
+%problem(3,A,B),tan_and_white(A,B).
+%problem(4,A,B),tan_and_white(A,B).
+
 
 
 
---
+++ TP2/other.pl
@@ -0,0 +1,226 @@
+:- use_module(library(clpfd)).
+:- use_module(library(lists)).     
+            
+problem(1,
+
+    [	[_,_,_,_],
+	[_,_,_,_],
+	[_,_,_,_],
+	[_,_,_,_]],
+	
+   [[4,4,5,6],
+	[3,3,5,6],
+	[2,1,6,7],
+	[0,2,7,0]]		
+		).
+		
+		
+problem(2,
+
+    [	[_,_],
+	[_,_]],
+	
+    [	[4,5],
+	[1,7]]		
+		).
+		
+problem(3,
+
+    [	[_,_,_,_],
+	[_,_,_,_]],
+	
+    [	[4,4,4,4],
+	[0,0,0,0]]		
+		).
+
+
+problem(4,
+
+    [	[_,_],
+	[_,_],
+	[_,_]],
+	
+    [	[3,5],
+	[1,7],
+	[2,6]]		
+		).
+
+length_(Length, List) :- length(List, Length).
+
+list2matrix(List, RowSize, Matrix) :-
+    length(List, L),
+    HowManyRows is L div RowSize,
+    length(Matrix, HowManyRows),
+    maplist(length_(RowSize), Matrix),
+    append(Matrix, List).
+
+tan_and_white(Blank,Dir):-
+	append(Blank,BlankS),
+	append(Dir,DirS),
+	length(DirS,Size),
+	length(BlankS,SB),
+	Size = SB,
+	length(Blank,L),
+	nth0(0,Blank,Linha),
+	length(Linha,C),
+	domain(BlankS,0,1),
+	
+	list2matrix(BlankS,C,Matrix),
+
+	tan_and_white(Matrix,Dir,0,0,Size,C,L).
+
+
+
+tan_and_white(Blank,Dir,X,Y,Size,Col,Line):- 
+	X>=Col.
+tan_and_white(Blank,Dir,X,Y,Size,Col,Line):- 
+	Y>=Line.
+tan_and_white(Blank,Dir,X,Y,Size,Col,Line):-
+	nth0(Y,Blank,BlankR),
+	nth0(X,BlankR,B),
+
+	nth0(Y,Dir,DirR),
+	nth0(X,DirR,D),
+
+	get_list(B,D,Blank,X,Y,Col,Line,Lista),
+
+	P is Y * Col,
+	P1 is P + X,
+	P2 is P1 + 1,
+	X1 is P2 mod Col,
+	Y1 is div(P2,Col), 
+
+	tan_and_white(Blank,Dir,X1,Y1,Size,Col,Line).
+
+get_list(Tan,Dir,Blank,X,Y,Cols,Lines,Lista):-
+	Tan #= 0,
+	get_list(Dir,Blank,X,Y,Cols,Lines,Lista),
+	length(Lista,LL),
+	S #= LL - 2,
+	global_cardinality(Lista,[0-2,1-S]).
+get_list(Tan,Dir,Blank,X,Y,Cols,Lines,Lista):-
+	Tan #= 1,
+	get_list(Dir,Blank,X,Y,Cols,Lines,Lista),
+	length(Lista,LL),
+	S #= LL - 3,
+	global_cardinality(Lista,[0-S,1-3]).
+
+
+get_list(0,Blank,X,Y,Cols,Lines,Lista):-
+	Y<0,
+	Lista = [].
+get_list(0,Blank,X,Y,Cols,Lines,Lista):-
+	Y >= 0,
+	Y1 is Y - 1,
+	get_list(0,Blank,X,Y1,Cols,Lines,OutLista),
+	nth0(Y,Blank,Row),
+	nth0(X,Row,Elem),
+	Lista = [Elem|OutLista]. 
+
+get_list(1,Blank,X,Y,Cols,Lines,Lista):-
+	Y<0,
+	Lista = [].
+get_list(1,Blank,X,Y,Cols,Lines,Lista):-
+	X>=Cols,
+	Lista = [].
+get_list(1,Blank,X,Y,Cols,Lines,Lista):-
+	Y >= 0,
+	X < Cols,
+	Y1 is Y - 1,
+	X1 is X + 1,
+	get_list(1,Blank,X1,Y1,Cols,Lines,OutLista),
+	nth0(Y,Blank,Row),
+	nth0(X,Row,Elem),
+	Lista = [Elem|OutLista]. 
+
+get_list(2,Blank,X,Y,Cols,Lines,Lista):-
+	X>=Cols,
+	Lista = [].
+get_list(2,Blank,X,Y,Cols,Lines,Lista):-
+	X < Cols,
+	X1 is X + 1,
+	get_list(2,Blank,X1,Y,Cols,Lines,OutLista),
+	nth0(Y,Blank,Row),
+	nth0(X,Row,Elem),
+	Lista = [Elem|OutLista].
+
+get_list(3,Blank,X,Y,Cols,Lines,Lista):-
+	Y>=Lines,
+	Lista = [].
+get_list(3,Blank,X,Y,Cols,Lines,Lista):-
+	X>=Cols,
+	Lista = [].
+get_list(3,Blank,X,Y,Cols,Lines,Lista):-
+	Y < Lines,
+	X < Cols,
+	Y1 is Y + 1,
+	X1 is X + 1,
+	get_list(3,Blank,X1,Y1,Cols,Lines,OutLista),
+	nth0(Y,Blank,Row),
+	nth0(X,Row,Elem),
+	Lista = [Elem|OutLista]. 
+
+get_list(4,Blank,X,Y,Cols,Lines,Lista):-
+	Y >= Lines,
+	Lista = [].
+get_list(4,Blank,X,Y,Cols,Lines,Lista):-
+	Y < Lines,
+	Y1 is Y + 1,
+	get_list(4,Blank,X,Y1,Cols,Lines,OutLista),
+	nth0(Y,Blank,Row),
+	nth0(X,Row,Elem),
+	Lista = [Elem|OutLista]. 
+
+get_list(5,Blank,X,Y,Cols,Lines,Lista):-
+	Y>=Lines,
+	Lista = [].
+get_list(5,Blank,X,Y,Cols,Lines,Lista):-
+	X < 0,
+	Lista = [].
+get_list(5,Blank,X,Y,Cols,Lines,Lista):-
+	Y < Lines,
+	X >= 0,
+	Y1 is Y + 1,
+	X1 is X - 1,
+	get_list(5,Blank,X1,Y1,Cols,Lines,OutLista),
+	nth0(Y,Blank,Row),
+	nth0(X,Row,Elem),
+	Lista = [Elem|OutLista]. 
+
+get_list(6,Blank,X,Y,Cols,Lines,Lista):-
+	X < 0,
+	Lista = [].
+get_list(6,Blank,X,Y,Cols,Lines,Lista):-
+	X >= 0,
+	X1 is X - 1,
+	get_list(6,Blank,X1,Y,Cols,Lines,OutLista),
+	nth0(Y,Blank,Row),
+	nth0(X,Row,Elem),
+	Lista = [Elem|OutLista].
+
+get_list(7,Blank,X,Y,Cols,Lines,Lista):-
+	X < 0,
+	Lista = [].
+get_list(7,Blank,X,Y,Cols,Lines,Lista):-
+	Y < 0,
+	Lista = [].
+get_list(7,Blank,X,Y,Cols,Lines,Lista):-
+	Y >= 0,
+	X >= 0,
+	Y1 is Y - 1,
+	X1 is X - 1,
+	get_list(7,Blank,X1,Y1,Cols,Lines,OutLista),
+	nth0(Y,Blank,Row),
+	nth0(X,Row,Elem),
+	Lista = [Elem|OutLista]. 
+
+%spy(setC),problem(2,A,B),tan_and_white(A,B).
+%problem(1,A,B),tan_and_white(A,B).
+%problem(3,A,B),tan_and_white(A,B).
+%problem(4,A,B),tan_and_white(A,B).
+
+
+
+
+
+
